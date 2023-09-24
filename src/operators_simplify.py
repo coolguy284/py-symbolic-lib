0717de8aeb97b6afceae006aa4eb1e728c5cfcac
coolguy284 <coolguy284v64@gmail.com>
2023-09-23 23:20:42 -0400

    update todo list
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

---
+++ src/operators_simplify.py
@@ -0,0 +1,6 @@
+def operator_simplify(object_one,
+  remove_identiy_components = False,
+  collapse_constants = True,
+  merge_nested_groups_same_type = True):
+  if remove_identiy_components:
+    pass
